cd1705ce745e8550560d6dba4013cfda4cd39628
Mohamad Chalhoub <mchalhoub42@gmail.com>
2019-05-13 17:30:45 +0300

    Creating User class and adding a user in the RegisterActivity

--- app/src/main/java/com/example/sic/RegisterActivity.java
+++ app/src/main/java/com/example/sic/RegisterActivity.java
@@ -1,11 +1,19 @@
 package com.example.sic;
 
+import android.support.annotation.NonNull;
 import android.support.design.widget.TextInputLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.FirebaseDatabase;
 
 public class RegisterActivity extends AppCompatActivity {
     EditText ufirstname, ulastname, uemail, upassword, uconfpassword, ucontactno;
@@ -13,11 +21,13 @@ public class RegisterActivity extends AppCompatActivity {
     TextInputLayout userFirstNameWrapper, userLastNameWrapper, userEmailWrapper, userPasswordWrapperm,
             userConfPasswordWrapper, userContactNoWrapper;
 
+    private FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
+        mAuth = FirebaseAuth.getInstance();
         ufirstname = findViewById(R.id.userFirstName);
         ulastname = findViewById(R.id.userLastName);
         uemail = findViewById(R.id.userEmailAddress);
@@ -37,46 +47,74 @@ public class RegisterActivity extends AppCompatActivity {
         btnRegister.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                String firstname = ufirstname.getText().toString().trim();
-                String lastname = ulastname.getText().toString().trim();
-                String email = uemail.getText().toString().trim();
-                String password = upassword.getText().toString().trim();
-                String confpassword = uconfpassword.getText().toString().trim();
-                String contactno = ucontactno.getText().toString().trim();
-                if(firstname.isEmpty()){
-                    userFirstNameWrapper.setError("Enter First Name");
-                    userFirstNameWrapper.requestFocus();
-                    return;
-                }
-                if(lastname.isEmpty()){
-                    userLastNameWrapper.setError("Enter Last Name");
-                    userLastNameWrapper.requestFocus();
-                    return;
-                }
-                if(email.isEmpty()){
-                    userEmailWrapper.setError("Enter Email");
-                    userEmailWrapper.requestFocus();
-                    return;
-                }
-                if(password.isEmpty()){
-                    userPasswordWrapperm.setError("Enter Password");
-                    userPasswordWrapperm.requestFocus();
-                    return;
-                }
-                if(confpassword.isEmpty()){
-                    userConfPasswordWrapper.setError("Enter Confirm Password");
-                    userConfPasswordWrapper.requestFocus();
-                    return;
-                }
-                if(!password.equals(confpassword)){
-                    userConfPasswordWrapper.setError("Password didn't match");
-                    userConfPasswordWrapper.requestFocus();
-                    return;
-                }
-                if(contactno.isEmpty()){
-                    userContactNoWrapper.setError("Enter Contact Number");
-                    userContactNoWrapper.requestFocus();
-                    return;
+                if(mAuth.getCurrentUser()!= null){
+
+                }else{
+
+                    final String firstname = ufirstname.getText().toString().trim();
+                    final String lastname = ulastname.getText().toString().trim();
+                    final String email = uemail.getText().toString().trim();
+                    String password = upassword.getText().toString().trim();
+                    String confpassword = uconfpassword.getText().toString().trim();
+                    final String contactno = ucontactno.getText().toString().trim();
+                    if(firstname.isEmpty()){
+                        userFirstNameWrapper.setError("Enter First Name");
+                        userFirstNameWrapper.requestFocus();
+                        return;
+                    }
+                    if(lastname.isEmpty()){
+                        userLastNameWrapper.setError("Enter Last Name");
+                        userLastNameWrapper.requestFocus();
+                        return;
+                    }
+                    if(email.isEmpty()){
+                        userEmailWrapper.setError("Enter Email");
+                        userEmailWrapper.requestFocus();
+                        return;
+                    }
+                    if(password.isEmpty()){
+                        userPasswordWrapperm.setError("Enter Password");
+                        userPasswordWrapperm.requestFocus();
+                        return;
+                    }
+                    if(confpassword.isEmpty()){
+                        userConfPasswordWrapper.setError("Enter Confirm Password");
+                        userConfPasswordWrapper.requestFocus();
+                        return;
+                    }
+                    if(!password.equals(confpassword)){
+                        userConfPasswordWrapper.setError("Password didn't match");
+                        userConfPasswordWrapper.requestFocus();
+                        return;
+                    }
+                    if(contactno.isEmpty()){
+                        userContactNoWrapper.setError("Enter Contact Number");
+                        userContactNoWrapper.requestFocus();
+                        return;
+                    }
+                    mAuth.createUserWithEmailAndPassword(email, password).addOnCompleteListener(new OnCompleteListener<AuthResult>() {
+                        @Override
+                        public void onComplete(@NonNull Task<AuthResult> task) {
+                            if(task.isSuccessful()){
+                                User user = new User(firstname, lastname , email, contactno);
+                                FirebaseDatabase .getInstance().getReference("Users")
+                                        .child(FirebaseAuth.getInstance().getCurrentUser().getUid())
+                                        .setValue(user).addOnCompleteListener(new OnCompleteListener<Void>() {
+                                    @Override
+                                    public void onComplete(@NonNull Task<Void> task) {
+                                        if(task.isSuccessful()){
+                                            Toast.makeText(RegisterActivity.this, "User created successfuly.", Toast.LENGTH_LONG).show();
+                                        }else {
+                                            Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                                        }
+                                    }
+                                });
+                            }
+                            else {
+                                Toast.makeText(RegisterActivity.this, task.getException().getMessage(), Toast.LENGTH_LONG).show();
+                            }
+                        }
+                    });
                 }
             }
         });
