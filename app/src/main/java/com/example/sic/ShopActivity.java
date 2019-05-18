79d659af9dec135d6ac18d424c8e6d1b88e5653a
Mohamad Chalhoub <mchalhoub42@gmail.com>
2019-05-18 11:20:46 +0300

    Adding the image Banner in the Shop Activity with some animation and changing the app icon (ic_launcher)

--- app/src/main/java/com/example/sic/ShopActivity.java
+++ app/src/main/java/com/example/sic/ShopActivity.java
@@ -2,12 +2,31 @@ package com.example.sic;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.ImageView;
+import android.widget.ViewFlipper;
 
 public class ShopActivity extends AppCompatActivity {
-
+    ViewFlipper imgBanner;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_shop);
+        imgBanner = findViewById(R.id.imgBanner);
+        int sliders[] = {
+                R.drawable.general_banner, R.drawable.electronic_banner, R.drawable.selfcare_banner
+        };
+        for(int slide : sliders)
+        {
+            bannerFlipper(slide);
+        }
+    }
+    public void bannerFlipper (int image){
+        ImageView imageView = new ImageView(this);
+        imageView.setImageResource(image);
+        imgBanner.addView(imageView);
+        imgBanner.setFlipInterval(4000);
+        imgBanner.setAutoStart(true);
+        imgBanner.setInAnimation(this, android.R.anim.fade_in);
+        imgBanner.setOutAnimation(this, android.R.anim.fade_out);
     }
 }
