5be85b425ad8a2988ad01bf1a61540ff15b1f457
Mohamad Chalhoub <mchalhoub42@gmail.com>
2019-05-30 08:53:33 +0300

    Adding a Navigation drawer to the Shop Activity.

--- app/src/main/java/com/example/sic/ShopActivity.java
+++ app/src/main/java/com/example/sic/ShopActivity.java
@@ -2,13 +2,19 @@ package com.example.sic;
 
 import android.support.annotation.NonNull;
 import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.NavigationView;
 import android.support.design.widget.Snackbar;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.GridLayoutManager;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.Toast;
@@ -30,7 +36,8 @@ import com.google.firebase.firestore.QuerySnapshot;
 import java.util.ArrayList;
 import java.util.List;
 
-public class ShopActivity extends AppCompatActivity {
+public class ShopActivity extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener{
     ViewFlipper imgBanner;
     private RecyclerView mRecycleView, cRecycleView;
     private PopularAdapter mAdaptar;
@@ -42,7 +49,10 @@ public class ShopActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_shop_main);
+        setContentView(R.layout.activity_shop_drawer);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+
         imgBanner = findViewById(R.id.imgBanner);
         db = FirebaseFirestore.getInstance();
         int sliders[] = {
@@ -63,6 +73,15 @@ public class ShopActivity extends AppCompatActivity {
                         .setAction("Action", null).show();
             }
         });
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
     }
     public void bannerFlipper (int image){
         ImageView imageView = new ImageView(this);
@@ -133,5 +152,30 @@ public class ShopActivity extends AppCompatActivity {
                     }
                 });
 
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.nav_camera) {
+            // Handle the camera action
+        } else if (id == R.id.nav_gallery) {
+
+        } else if (id == R.id.nav_slideshow) {
+
+        } else if (id == R.id.nav_manage) {
+
+        } else if (id == R.id.nav_share) {
+
+        } else if (id == R.id.nav_send) {
+
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
     }
 }
