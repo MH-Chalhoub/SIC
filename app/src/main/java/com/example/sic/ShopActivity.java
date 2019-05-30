bd6506ad446037d15ae5f9f1e6c28f512a92b3c1
Mohamad Chalhoub <mchalhoub42@gmail.com>
2019-05-30 19:47:04 +0300

    Adding onBackPressed method and a btn_favorite_black.xml

--- app/src/main/java/com/example/sic/ShopActivity.java
+++ app/src/main/java/com/example/sic/ShopActivity.java
@@ -178,4 +178,14 @@ public class ShopActivity extends AppCompatActivity
         drawer.closeDrawer(GravityCompat.START);
         return true;
     }
+
+    @Override
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
 }
