c9ba43cf79e991778cad57907bb3a320f5f2e7f3
Mohamad Chalhoub <mchalhoub42@gmail.com>
2019-06-11 07:01:35 +0300

    Setting onClickListener to Category item

---
+++ app/src/main/java/com/example/sic/ItemContentActivity.java
@@ -0,0 +1,13 @@
+package com.example.sic;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class ItemContentActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_item_content);
+    }
+}
